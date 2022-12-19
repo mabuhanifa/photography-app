72451c83e5791542199870414a5c1c2e3a550cc9
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-19 17:38:20 +0600

    context implemented

--- pages/items/index.js
+++ pages/items/index.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { useApp } from "../../components/appContext";
 
 export default function index() {
+  const state = useApp();
+  console.log(state)
   return <div>Items</div>;
 }
