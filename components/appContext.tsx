0a685cf91fb0e357e49c7c8285c4eded80df6471
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-19 17:30:50 +0600

    Context.Provider

--- components/appContext.tsx
+++ components/appContext.tsx
@@ -1,6 +1,15 @@
+import React, { createContext } from "react";
 
-export default function AppContext() {
+const Context = createContext({});
+
+type ContextType = { children: React.ReactNode }
+export default function AppContext({ children }: ContextType) {
+    const store = {
+        a: "hello"
+    }
     return (
-        <div>A</div>
+        <Context.Provider value={store}>{
+            children
+        }</Context.Provider>
     )
 }
