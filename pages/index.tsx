0835f922574992996c15e88d5f2dfc31eaf53413
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-19 16:27:54 +0600

    ''

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import { useState } from 'react';
 
 const Home: NextPage = () => {
   const [state, setState] = useState(0);
+
   const btnClass = "bg-blue-700 px-5 rounded text-white";
+
   return (
     <div className="flex min-h-screen  items-center justify-center py-2 gap-10">
       {state}
