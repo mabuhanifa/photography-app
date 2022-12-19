a4f9bef98930c1b23ec80391165a9d9ae35b314a
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-19 16:51:53 +0600

    globals.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
-
+import '../styles/globals.css'
 export default function Homepage() {
     return (
-        <div>Home</div>
+        <div>
+            <div>
+                <h2 className="text-gray-700">
+                    tailwind
+                </h2>
+            </div>
+        </div>
     )
 }
