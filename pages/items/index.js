a48228ceb2c261dfcf705820732cbdffafa5bdd9
mabuhanifa <moh.abuhanifa@gmail.com>
2025-04-04 13:49:42 +0600

    app cleared

--- pages/items/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useApp } from "../../components/appContext";
-
-export default function index() {
-  const state = useApp();
-  console.log(state);
-  return <div>Items</div>;
-}
